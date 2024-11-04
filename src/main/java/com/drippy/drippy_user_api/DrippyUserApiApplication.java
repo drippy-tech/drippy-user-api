c6b8caab8d4fedb65ff107659d19bc11c6e125b4
Robson Paixão <robson.emanuelpx@gmail.com>
2024-11-04 19:20:43 -0300

    chore: initial commit

---
+++ src/main/java/com/drippy/drippy_user_api/DrippyUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.drippy.drippy_user_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrippyUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrippyUserApiApplication.class, args);
+	}
+
+}
